b56e852f0295634474d74685c55a62936c681c0e
jordanstark <jordan.r.stark@gmail.com>
2022-04-14 20:34:01 -0400

    updated figure to include inset of NE

--- fig_vmc_stack.R
+++ fig_vmc_stack.R
@@ -35,62 +35,28 @@
     names(summary_stack) <- c("median","maximum","minimum","quantile_025","quantile_975","SD","mean","range","range_95","CV")
 
 #### plot results ####
-pix <- 1e3 # max number of pixels to plot for each map, set to 1e7 or 1e8 for final figs or lower to test formatting
+pix <- 1e8 # max number of pixels to plot for each map, set to 1e7 or 1e8 for final figs or lower to test formatting
 
     
 scalebar_data <- data.frame(x=230000,xend=240000,y=3955000)
-    
-PlotFunc <- function(rstack){
-  minval <- min(cellStats(rstack,min))
-  maxval <- max(cellStats(rstack,max))
-  
-  plots <- list()
-  for(i in 1:nlayers(rstack)){
-    
-  
-    plots[[i]] <-  gplot(rstack[[i]],maxpixels=pix)+
-      geom_tile(aes(fill=value)) +
 
-      theme_void() +
-      scale_fill_distiller(name="",
-                           palette="RdYlBu",
-                           direction=-1,
-                           na.value="white",
-                           trans="logit",
-                           limits=c(minval,maxval),
-                           breaks=c(0.05,0.25,0.45,0.65)) +
-      theme(legend.key.height=unit(0.02,"npc"),
-            legend.key.width=unit(0.06,"npc"),
-            legend.position="bottom",
-            text=element_text(size=10)) +
-      labs(x="",y="",title=names(rstack)[i]) +
-      coord_fixed(expand=F) 
 
+ne.ext <- extent(290000,314000,3949000,3964000)
+scalebar_data_zoom <- data.frame(x=292000,xend=293000,y=3960500)
+
+
+ne.med <- crop(medr,ne.ext)
+ne.rng95 <- crop(medr,ne.ext)
+ne.cv <- crop(cv,ne.ext)
+
+PlotSingle <- function(r,title="",pal="RdYlBu",logittrans=T,dir=-1,scalebar=F,ne=F,legend=F){
+  minval <- cellStats(r,min)
+  maxval <- cellStats(r,max)
+  
+  if(ne){
+    r <- crop(r,ne.ext)
   }
   
-  plots[[1]] <- plots[[1]] +
-    geom_segment(data=scalebar_data,aes(x=x,xend=xend,y=y,yend=y),
-                 arrow=arrow(angle=90,ends="both",length=unit(0.02,"npc")))+
-    geom_text(aes(x=mean(c(scalebar_data$x,scalebar_data$xend)),
-                  y=scalebar_data$y+3000,label="10 km"),size=3) 
-  
-  plots <- wrap_plots(plots,guides="collect",ncol=1) &
-            theme(legend.position="bottom")
-  
-  return(plots)
-  
-}
-
-#PlotFunc(summary_stack)
-
-
-ggsave(plot=PlotFunc(summary_stack),filename=paste0(fig_path,"summer_vmc_summary.tiff"))
-
-
-pix <- 1e7
-
-PlotSingle <- function(r,title="",pal="RdYlBu",logittrans=T,dir=-1,scalebar=F){
-
     plot <-  gplot(r,maxpixels=pix)+
       geom_tile(aes(fill=value)) +
       theme_void() +
@@ -107,23 +73,40 @@ PlotSingle <- function(r,title="",pal="RdYlBu",logittrans=T,dir=-1,scalebar=F){
                            direction=-dir,
                            na.value="white",
                            trans="logit",
-                           breaks=c(0.05,0.25,0.45,0.65)) 
+                           breaks=c(0.05,0.25,0.45,0.65),
+                           limits=c(minval,maxval))
   } else {
     plot <- plot +
       scale_fill_distiller(name="",
                            palette=pal,
                            direction=-dir,
                            na.value="white",
-                           breaks=c(0,0.1,0.2,0.3,0.4,0.5)) 
+                           breaks=c(0,0.1,0.2,0.3,0.4,0.5),
+                           limits=c(minval,maxval)) 
   }
     
     
   if(scalebar){
-    plot <- plot +
-      geom_segment(data=scalebar_data,aes(x=x,xend=xend,y=y,yend=y),
+    if(!ne){
+      plot <- plot +
+        geom_segment(data=scalebar_data,aes(x=x,xend=xend,y=y,yend=y),
+                     arrow=arrow(angle=90,ends="both",length=unit(0.02,"npc")))+
+        geom_text(aes(x=mean(c(scalebar_data$x,scalebar_data$xend)),
+                      y=scalebar_data$y+3000,label="10 km"),size=3) 
+    } else if(ne){
+      
+     plot <- plot +
+      geom_segment(data=scalebar_data_zoom,aes(x=x,xend=xend,y=y,yend=y),
                    arrow=arrow(angle=90,ends="both",length=unit(0.02,"npc")))+
-      geom_text(aes(x=mean(c(scalebar_data$x,scalebar_data$xend)),
-                    y=scalebar_data$y+3000,label="10 km"),size=3) 
+      geom_text(aes(x=mean(c(scalebar_data_zoom$x,scalebar_data_zoom$xend)),
+                    y=scalebar_data_zoom$y+1000,label="1 km"),size=3) 
+    }
+
+  }
+    
+  if(!legend){
+    plot <- plot +
+              theme(legend.position="none")
   }
   
   return(plot)
@@ -131,10 +114,13 @@ PlotSingle <- function(r,title="",pal="RdYlBu",logittrans=T,dir=-1,scalebar=F){
 }
 
 medplot <- PlotSingle(medr,title="median",scalebar=T)
-rngplot <- PlotSingle(rng95,title="range of middle 95%",pal="PiYG",dir=1,logittrans=T)
-cvplot <- PlotSingle(cv,title="coefficient of variation",pal="BrBG",dir=-1,logittrans=T)
+medplot_ne <- PlotSingle(ne.med,ne=T,scalebar=T,legend=T)
+rngplot <- PlotSingle(rng95,title="range of middle 95%",pal="PiYG",dir=1)
+rngplot_ne <- PlotSingle(rng95,pal="PiYG",dir=1,ne=T,legend=T)
+cvplot <- PlotSingle(cv,title="coefficient of variation",pal="BrBG",dir=-1)
+cvplot_ne <- PlotSingle(cv,pal="BrBG",dir=-1,ne=T,legend=T)
 
-#medplot/rngplot/cvplot
+full_plot <- (medplot|medplot_ne)/(rngplot|rngplot_ne)/(cvplot|cvplot_ne)
 
 
-ggsave(plot=(medplot/rngplot/cvplot),filename=paste0(fig_path,"summer_vmc_summary_fig.tiff"),width=6,height=9)
+ggsave(plot=full_plot,filename=paste0(fig_path,"summer_vmc_summary_fig2.tiff"),width=7,height=7)
